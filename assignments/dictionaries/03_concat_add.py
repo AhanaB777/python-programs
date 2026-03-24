ed5da4397d98e050d98eae62803d0d612d7fcb05
AhanaB777 <217305720+AhanaB777@users.noreply.github.com>
2026-03-25 00:26:03 +0530

    changes

--- assignments/dictionaries/03_concat_add.py
+++ assignments/dictionaries/03_concat_add.py
@@ -1,14 +1,16 @@
 """Concat and add"""
 
-dic1={1:10,2:20,4:6}
-dic2={3:30,4:40,5:2}
-dic3={5:50,6:60}
-d4=dic1
-d5=dic2|dic3
+dic1 = {1:10, 2:20, 4:6}
+dic2 = {3:30, 4:40, 5:2}
+dic3 = {5:50, 6:60}
 
-for i in d5.keys():
-    if i in d4.keys():
-        d4[i]+=d5[i]
-    else:
-        d4[i]=d5[i]
-print(d4) 
+result = {}
+
+for d in (dic1, dic2, dic3):
+    for k, v in d.items():
+        if k in result:
+            result[k] += v
+        else:
+            result[k] = v
+
+print(result)
